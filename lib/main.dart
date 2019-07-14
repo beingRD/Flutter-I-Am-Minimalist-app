07c84a1ea47e19826c83a16d4795c577216b4aba
“Rishabh <devrishabh@Rishabhs-MacBook-Pro.local>
2019-07-14 12:13:56 +0200

    initial

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      appBar: AppBar(
+        title: Text(
+          'I am a Minimalist!',
+          style: TextStyle(
+            color: Colors.amber,
+            fontSize: 26.0,
+          ),
+        ),
+        elevation: defaultTargetPlatform == TargetPlatform.iOS ? 0.0 : 6.0,
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image.asset(
+          'images/clever-man-100.png',
+          fit: BoxFit.fill,
+          width: 140.0,
+          height: 140.0,
+        ),
+      ),
+    ),
+  ));
+}
